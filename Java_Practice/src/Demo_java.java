455e439fa45a904e9bce851c1573e925560187cf
Dipak Nandeshwar <dipaknandeshwar94@gmail.com>
2022-02-04 00:06:31 +0530

    Add files via upload

---
+++ Java_Practice/src/Demo_java.java
@@ -0,0 +1,14 @@
+
+// Importing classes from packages
+import java.io.*;
+
+// Main class
+public class Demo_java {
+
+    // Main driver method
+    public static void main(String[] args)
+    {
+        // Print statement
+        System.out.println("Welcome to the World");
+    }
+}
