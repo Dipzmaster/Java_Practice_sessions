48abf2c342755c0631345a62fe63459758bc4103
Dipak Nandeshwar <dipaknandeshwar94@gmail.com>
2022-02-06 00:12:18 +0530

    Add files via upload

---
+++ Java_Practice/src/Bool.java
@@ -0,0 +1,11 @@
+
+// Boolean data type
+
+class Bool {
+    public static void main(String args[])
+    {
+        boolean b = true;
+        if (b == true)
+            System.out.println("Hi Buddy");
+    }
+}
