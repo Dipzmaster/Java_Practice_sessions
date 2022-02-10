9ebedee4cc2b524242114af3860225041a2fb270
Dipak Nandeshwar <dipaknandeshwar94@gmail.com>
2022-02-10 20:33:44 +0530

    Add files via upload

---
+++ Java_Practice/src/For_loop_1.java
@@ -0,0 +1,12 @@
+
+class For_loop_1 {
+    public static void main(String args[])
+    {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+        System.out.println("Hello");
+        int i = 20;
+        System.out.println(i);
+    }
+}
